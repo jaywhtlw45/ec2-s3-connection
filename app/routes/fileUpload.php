144ac9592c58facf92d115b5ef60e1c53c9dbd7b
Jason <jason@solutionsbydesign.com>
2025-09-09 11:36:08 -0700

    add

--- app/routes/fileUpload.php
+++ app/routes/fileUpload.php
@@ -1,10 +1,14 @@
 <?php
 if ($_SERVER['REQUEST_METHOD']==='POST') {
     error_log(json_encode($_POST));
+    
+    if (isset($_POST["file"])){
+        error_log("a file exists");
+    }
     // Response
     http_response_code(200);
     header('Content-Type: application/json');
-    $response = ["message"=>"failure", "car"=>"toyota", "error_log_location"=> $log_location];
+    $response = ["message"=>"failure", "car"=>"toyota"];
     echo json_encode($response);
 }
 ?>
