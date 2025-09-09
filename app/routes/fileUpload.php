8cdba93082acc8168fce642373da524654c059f0
Jason <jason@solutionsbydesign.com>
2025-09-09 09:54:45 -0700

    add

--- app/routes/fileUpload.php
+++ app/routes/fileUpload.php
@@ -3,5 +3,8 @@ error_log("request recieved");
 if ($_SERVER['REQUEST_METHOD']==='POST') {
     error_log("post request recieved");
     http_response_code(200);
+    header('Content-Type: application/json');
+    $response = ["message"=>"success", "car"=>"toyota"];
+    echo json_encode($respone);
 }
 ?>
