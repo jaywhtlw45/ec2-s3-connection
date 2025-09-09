1fc80c5474253382c639486f2c5fe1453fa923c2
Jason <jason@solutionsbydesign.com>
2025-09-09 11:49:31 -0700

    add

--- app/routes/fileUpload.php
+++ app/routes/fileUpload.php
@@ -2,14 +2,20 @@
 if ($_SERVER['REQUEST_METHOD']==='POST') {
     error_log(json_encode($_POST));
     
+    $response = [];
     if (isset($_FILES["file"])){
         $file = $_FILES["file"];
-        error_log($file);
+        $response["filename"] = $file["name"];
+        $response["filetype"] = $file["type"];
+        $response["tempLocation"] = $file["temp_name"];
+        $response["size"] = $file["size"];
+        $response["error"] = $file["error"];
     }
+
     // Response
     http_response_code(200);
     header('Content-Type: application/json');
-    $response = ["message"=>"failure", "car"=>"toyota"];
+    $response["message"] = "success";
     echo json_encode($response);
 }
 ?>
