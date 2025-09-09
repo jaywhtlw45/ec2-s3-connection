70bce75dd9e95978e0070677ce3cde9bb645fe24
Jason <jason@solutionsbydesign.com>
2025-09-09 10:11:38 -0700

    add

--- app/routes/fileUpload.php
+++ app/routes/fileUpload.php
@@ -1,5 +1,7 @@
 <?php
 error_log("request recieved");
+    error_log("testing");
+
 if ($_SERVER['REQUEST_METHOD']==='POST') {
     error_log("testing");
     error_log("server: " . json_encode($_SERVER));
